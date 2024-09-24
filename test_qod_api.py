8982821a55da4bcc965b61ae0ce8b4b852d3a0a0
bwu-contour <bing.wu@chase.com>
2024-09-24 12:29:11 -0700

    add tests

---
+++ test_qod_api.py
@@ -0,0 +1,17 @@
+import pytest
+import requests
+
+ENDPOINT = "http://localhost:8080"
+
+@pytest.mark.repeat(10)  # Run the test 10 times
+def test_can_call_endpoint():
+    response = requests.get(ENDPOINT)
+    assert response.status_code == 200
+
+@pytest.mark.repeat(10)  # Run the test 10 times
+def test_get_daily_quote():
+    response = requests.get(ENDPOINT + "/daily")
+    assert response.status_code == 200
+    
+    #data = response.json();
+    #print(data)
