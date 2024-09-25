9cc818050019db4a245efaa12a8e047d5233697e
bwu-contour <bing.wu@chase.com>
2024-09-25 13:45:44 -0700

    handle HTTPS

--- test_qod_api.py
+++ test_qod_api.py
@@ -5,12 +5,18 @@ ENDPOINT = "http://localhost:8080"
 
 @pytest.mark.repeat(10)  # Run the test 10 times
 def test_can_call_endpoint():
-    response = requests.get(ENDPOINT, verify=False)
+    if "https" in ENDPOINT.lower():
+        response = requests.get(ENDPOINT, verify=False)
+    else:
+        response = requests.get(ENDPOINT)
     assert response.status_code == 200
 
 @pytest.mark.repeat(10)  # Run the test 10 times
 def test_get_daily_quote():
-    response = requests.get(ENDPOINT + "/daily", verify=False)
+    if "https" in ENDPOINT.lower():
+        response = requests.get(ENDPOINT + "/daily", verify=False)
+    else:
+        response = requests.get(ENDPOINT + "/daily")
     assert response.status_code == 200
     
     #data = response.json();
@@ -18,7 +24,10 @@ def test_get_daily_quote():
     
 @pytest.mark.repeat(10)  # Run the test 10 times
 def test_get_random_quote():
-    response = requests.get(ENDPOINT + "/random", verify=False)
+    if "https" in ENDPOINT.lower():
+        response = requests.get(ENDPOINT + "/random", verify=False)
+    else:
+        response = requests.get(ENDPOINT + "/random")
     assert response.status_code == 200
     
     #data = response.json();
